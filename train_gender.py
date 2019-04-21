2d17f0ca26feda9ba50a6b16265a3dc78c5c89aa
Kamal Rahimi Malekshan <rahimi.kamal@gmail.com>
2019-04-21 17:25:52 -0400

    Added over sampling for gender identifcation model and code cleanup

--- train_gender.py
+++ train_gender.py
@@ -1,22 +1,17 @@
 """
-Creates a convolutionaly neural network (CNN) in Tensorflow and trains the network
+Creates a convolutional neural network (CNN) model in Tensorflow and trains the model
 to identify gender of the person in an input image.
 """
 
-import cv2
-import numpy as np
 import tensorflow as tf
-
-import tensorflow as tf
-
-from imblearn.over_sampling import RandomOverSampler
-from collections import Counter
 
 from cvision_tools import ColorfretDataset, read_feret_data, detect_face, crop_face, convert_to_gray, resize_with_pad
 
+import numpy as np
 from sklearn.preprocessing import LabelEncoder
-
+from imblearn.over_sampling import RandomOverSampler
 import pickle
+
 
 image_height = 100
 image_width = 100
@@ -33,38 +28,67 @@ GENDER_ENCODER_PATH = "./model/gender_encoder.pickle"
 GENDER_PREDICTION_MODEL_PATH = "./model/gender_model"
 
 
-def main():
+def prepare_data():
+    """ Prepares training and test data for gender identification model from Colofret image dataset 
+    (Face area in each image is detected and cropped)  
+    Args:
+    Returns:
+        X_train: a numpy array of the train face image data
+        X_test: a numpy array of the test face image data
+        y_gender_train: a numpy array of the train gender lables
+        y_gender_test: a numpy array of the test gender lables
+    """
     dataset = ColorfretDataset()
     dataset.read()
-
-    X_train         = np.array(dataset.X_train)
-    X_test          = np.array(dataset.X_test)
-
+    X_train         = np.array(dataset.X_train).astype('float32')
+    X_test          = np.array(dataset.X_test).astype('float32')
+    X_train        /= 255
+    X_test         /= 255
+    
     y_gender_train  = np.array([y['gender'] for y in dataset.y_train])
     y_gender_test   = np.array([y['gender'] for y in dataset.y_test])
 
-    X_train         = X_train.astype('float32')
-    X_test          = X_test.astype('float32')
-    X_train        /= 255
-    X_test         /= 255
+    return X_train, X_test, y_gender_train, y_gender_test
 
+
+def create_gender_encoder(y_gender):
+    """ Creates a gender label encoder model
+    Args:
+        y_gender: a numpy array of gender labels
+    Returns:
+        gender_enc: a gender label encoder model
+    """
     gender_enc = LabelEncoder()
-    y_gender_train  = gender_enc.fit_transform(y_gender_train)
-    y_gender_test   = gender_enc.transform(y_gender_test)
+    gender_enc.fit(y_gender)
     pickle.dump(gender_enc, open(GENDER_ENCODER_PATH, 'wb'))
+    return gender_enc
 
-    num_gender_outputs = len(gender_enc.classes_)
+def over_sample(X, y):
+    random_over_sampler = RandomOverSampler(random_state=42)
+    nsamples, nx, ny, nz = X.shape
+    d2_X = X.reshape((nsamples,nx*ny*nz))
+    d2_X_os, y_os = random_over_sampler.fit_resample(np.array(d2_X), y)
+    X_os = d2_X.reshape(-1, nx, ny, nz)
+    #print(sorted(Counter(y_os).items()))
+    return X_os, y_os
 
-    gender_uniques, gender_counts = np.unique(y_gender_train, return_counts=True)
-    print(gender_enc.inverse_transform(gender_uniques))
-    print (gender_counts)
+def train_gender_model(X_train, X_test, y_train, y_test, gender_encoder):
+    """ Creates a convoluational neural network (CNN) and trains the model to identify gender
+    of an input face image
+    Args:
+        X_train: a numpy array of the train image data
+        X_test: a numpy array of the test image data
+        y_train: a numpy array of the train gender lables
+        y_test: a numpy array of the test gender lables
+    Returns:
+    """
 
+    num_gender_outputs = len(gender_encoder.classes_)
+    gender_uniques, gender_counts = np.unique(y_train, return_counts=True)
 
-    with tf.name_scope("cnn_gender_graph"):
+    with tf.name_scope("cnn_graph_gender"):
         X = tf.placeholder(tf.float32, shape = [None, image_height, image_width, image_n_channels], name="X")
-        #y = tf.placeholder(tf.float32, shape = [None, image_height, image_weight, image_n_channels])
         y = tf.placeholder(tf.int32, shape = [None])
-        loss_weight = tf.placeholder(tf.float32, shape = [None])
         training = tf.placeholder_with_default(False, shape=[], name='training')
 
         conv1 = tf.layers.conv2d(X, filters=8, kernel_size=4,
@@ -84,7 +108,7 @@ def main():
         pool4_flat = tf.reshape(pool4, shape=[-1, 32 * 5 * 5])
         pool4_flat_drop = tf.layers.dropout(pool4_flat, 0.5, training=training)
 
-        fc1 = tf.layers.dense(pool4_flat, 32, activation=tf.nn.relu, name="fc1")
+        fc1 = tf.layers.dense(pool4_flat_drop, 32, activation=tf.nn.relu, name="fc1")
 
         logits_gender = tf.layers.dense(fc1, num_gender_outputs, name="logits_gender")
         Y_proba_gender = tf.nn.softmax(logits_gender, name="Y_proba_gender")
@@ -95,7 +119,6 @@ def main():
     with tf.name_scope("train_gender"):
         xentropy_gender = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits_gender, labels=y)
         loss_gender = tf.reduce_mean(xentropy_gender)
-        weighted_loss_gender = tf.math.multiply(loss_gender, loss_weight)
         optimizer_gender = tf.train.AdamOptimizer(learning_rate=learning_rate) # beta1=0.8
         #optimizer = tf.train.MomentumOptimizer(learning_rate=0.00005, momentum=0.9, use_nesterov=True)
         training_op_gender = optimizer_gender.minimize(loss_gender, global_step=global_step)
@@ -115,15 +138,28 @@ def main():
             n_it = X_train.shape[0]//batch_size
             for it in range(n_it):
                 X_batch = X_train[it*batch_size:(it+1)*batch_size,:,:,:]
-                y_batch = y_gender_train[it*batch_size:(it+1)*batch_size]
-                gender_weight_batch =  [1/gender_counts[i] for i in y_batch] 
-                train_gender_sess.run(training_op_gender, feed_dict={X: X_batch, y: y_batch, training: True, loss_weight:gender_weight_batch})
-            acc_batch = accuracy_gender.eval(feed_dict={X: X_batch, y: y_batch, loss_weight:np.ones(len(y_batch))})
-            acc_train = accuracy_gender.eval(feed_dict={X: X_train[1:100], y: y_gender_train[1:100], loss_weight:np.ones(100)})
-            acc_test = accuracy_gender.eval(feed_dict={X: X_test, y: y_gender_test, loss_weight:np.ones(len(y_gender_test))})
+                y_batch = y_train[it*batch_size:(it+1)*batch_size]
+                train_gender_sess.run(training_op_gender, feed_dict={X: X_batch, y: y_batch, training: True})
+            
+            acc_batch = accuracy_gender.eval(feed_dict={X: X_batch, y: y_batch})
+            acc_train = accuracy_gender.eval(feed_dict={X: X_train[1:100], y: y_train[1:100]})
+            acc_test = accuracy_gender.eval(feed_dict={X: X_test, y: y_test})
             print(epoch, "Last batch accuracy:", acc_batch, "Train accuracy:", acc_train, "Test accuracy:", acc_test)
 
             save_path_gender = saver.save(train_gender_sess, GENDER_PREDICTION_MODEL_PATH)
+
+
+def main():
+    X_train, X_test, y_gender_train, y_gender_test = prepare_data()
+    
+    gender_encoder = create_gender_encoder(y_gender_train)
+    
+    y_train = gender_encoder.transform(y_gender_train)
+    y_test  = gender_encoder.transform(y_gender_test)
+    
+    X_train, y_train = over_sample(X_train, y_train)
+
+    train_gender_model(X_train, X_test, y_train, y_test, gender_encoder)
 
 
 if __name__ == "__main__":
--- train_race.py
+++ train_race.py
@@ -3,25 +3,15 @@ Creates a convolutionaly neural network (CNN) in Tensorflow and trains the netwo
 to identify the race of the person in an input image.
 """
 
-import cv2
-import numpy as np
 import tensorflow as tf
-
-import tensorflow as tf
-
-from imblearn.over_sampling import RandomOverSampler
-from collections import Counter
-#import keras
-#from keras import backend as K
-
-#tf_sess = tf.Session()
-#K.set_session(tf_sess)
 
 from cvision_tools import ColorfretDataset, read_feret_data, detect_face, crop_face, convert_to_gray, resize_with_pad
 
+import numpy as np
 from sklearn.preprocessing import LabelEncoder
-
+from imblearn.over_sampling import RandomOverSampler
 import pickle
+
 
 image_height = 100
 image_width = 100
@@ -31,19 +21,30 @@ n_epochs = 300
 batch_size = 10
 
 initial_learning_rate = 0.001
-decay_steps = 20000
+decay_steps = 4000
 decay_rate = 1/2
 
 RACE_ENCODER_PATH = "./model/race_encoder.pickle"
 RACE_PREDICTION_MODEL_PATH = "./model/race_model"
 
 
-def main():
+def prepare_race_data():
+    """ Prepares training and test data for race identification model from Colofret image dataset 
+    (Face area in each image is detected and cropped)  
+    Args:
+    Returns:
+        X_train: a numpy array of the train face image data
+        X_test: a numpy array of the test face image data
+        y_race_train: a numpy array of the train race lables
+        y_race_test: a numpy array of the test race lables
+    """
     dataset = ColorfretDataset()
     dataset.read()
 
-    X_train         = np.array(dataset.X_train)
-    X_test          = np.array(dataset.X_test)
+    X_train         = np.array(dataset.X_train).astype('float32')
+    X_test          = np.array(dataset.X_test).astype('float32')
+    X_train        /= 255
+    X_test         /= 255
 
     y_race_train    = np.array([y['race'] for y in dataset.y_train])
     y_race_test     = np.array([y['race'] for y in dataset.y_test])
@@ -51,32 +52,46 @@ def main():
     y_race_train    = [y if (y != 'Native-American' and y != 'Pacific-Islander' and y != 'Asian-Southern') else 'Other' for y in  y_race_train ]
     y_race_test     = [y if (y != 'Native-American' and y != 'Pacific-Islander' and y != 'Asian-Southern') else 'Other' for y in  y_race_test ] 
 
-    X_train         = X_train.astype('float32')
-    X_test          = X_test.astype('float32')
-    X_train        /= 255
-    X_test         /= 255
+    return X_train, X_test, y_race_train, y_race_test
 
-    race_enc   = LabelEncoder()
-    y_race_train  = race_enc.fit_transform(y_race_train)
-    y_race_test   = race_enc.transform(y_race_test)
+
+def create_race_encoder(y_race):
+    """ Creates a race label encoder model
+    Args:
+        y_race: a numpy array of race labels
+    Returns:
+        race_enc: a race label encoder model
+    """
+    race_enc = LabelEncoder()
+    race_enc.fit(y_race)
     pickle.dump(race_enc, open(RACE_ENCODER_PATH, 'wb'))
+    return race_enc
 
-    num_race_outputs = len(race_enc.classes_)
-
-    race_uniques, race_counts = np.unique(y_race_train, return_counts=True)
-    print(race_enc.inverse_transform(race_uniques))
-    print (race_counts)
-
+def over_sample(X, y):
     random_over_sampler = RandomOverSampler(random_state=42)
-    nsamples, nx, ny, nz = X_train.shape
-    d2_X_train = X_train.reshape((nsamples,nx*ny*nz))
-    d2_X_train, y_race_train = random_over_sampler.fit_resample(np.array(d2_X_train), y_race_train)
-    X_train = d2_X_train.reshape(-1, nx, ny, nz)
+    nsamples, nx, ny, nz = X.shape
+    d2_X = X.reshape((nsamples,nx*ny*nz))
+    d2_X_os, y_os = random_over_sampler.fit_resample(np.array(d2_X), y)
+    X_os = d2_X.reshape(-1, nx, ny, nz)
+    #print(sorted(Counter(y_os).items()))
+    return X_os, y_os
 
-    print(sorted(Counter(y_race_train).items()))
 
+def train_race_model(X_train, X_test, y_train, y_test, race_encoder):
+    """ Creates a convoluational neural network (CNN) and trains the model to identify race
+    of an input face image
+    Args:
+        X_train: a numpy array of the train image data
+        X_test: a numpy array of the test image data
+        y_train: a numpy array of the train race lables
+        y_test: a numpy array of the test race lables
+    Returns:
+    """
 
-    with tf.name_scope("cnn_race_graph"):
+    num_race_outputs = len(race_encoder.classes_)
+    race_uniques, race_counts = np.unique(y_train, return_counts=True)
+
+    with tf.name_scope("cnn_graph_race"):
         X = tf.placeholder(tf.float32, shape = [None, image_height, image_width, image_n_channels], name="X")
         #y = tf.placeholder(tf.float32, shape = [None, image_height, image_weight, image_n_channels])
         y = tf.placeholder(tf.int32, shape = [None])
@@ -89,7 +104,7 @@ def main():
 
         pool2 = tf.nn.max_pool(conv1, ksize=[1, 5, 5, 1], strides=[1, 5, 5, 1], padding="SAME")
 
-        pool2_drop = tf.layers.dropout(pool2, 0.05, training=training)
+        pool2_drop = tf.layers.dropout(pool2, 0.1, training=training)
 
         conv3 = tf.layers.conv2d(pool2_drop, filters=32, kernel_size=4,
                                 strides=1, padding='SAME',
@@ -100,13 +115,13 @@ def main():
         shape = tf.shape(pool4)
 
         pool4_flat = tf.reshape(pool4, shape=[-1, 32 * 5 * 5])
-        pool4_flat_drop = tf.layers.dropout(pool4_flat, 0.1, training=training)
+        pool4_flat_drop = tf.layers.dropout(pool4_flat, 0.5, training=training)
 
         fc1 = tf.layers.dense(pool4_flat_drop, 32, name="fc1")
         bn1 = tf.layers.batch_normalization(fc1, training=training, momentum=0.9)
         bn1_act = tf.nn.relu(bn1)
 
-        logits_race = tf.layers.dense(fc1, num_race_outputs, name="logits_race")
+        logits_race = tf.layers.dense(bn1_act, num_race_outputs, name="logits_race")
         Y_proba_race = tf.nn.softmax(logits_race, name="Y_proba_race")
 
         global_step = tf.Variable(0, trainable=False, name="global_step")
@@ -115,7 +130,6 @@ def main():
     with tf.name_scope("train_race"):
         xentropy_race = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits_race, labels=y)
         loss_race = tf.reduce_mean(xentropy_race)
-        weighted_loss_race = tf.math.multiply(loss_race, loss_weight)
         optimizer_race = tf.train.AdamOptimizer(learning_rate=learning_rate) # beta1=0.8
         #optimizer = tf.train.MomentumOptimizer(learning_rate=0.00005, momentum=0.9, use_nesterov=True)
         training_op_race = optimizer_race.minimize(loss_race, global_step=global_step)
@@ -135,16 +149,28 @@ def main():
             n_it = X_train.shape[0]//batch_size
             for it in range(n_it):
                 X_batch = X_train[it*batch_size:(it+1)*batch_size,:,:,:]
-                y_batch = y_race_train[it*batch_size:(it+1)*batch_size]
-                race_weight_batch =  [1/race_counts[i] for i in y_batch] 
-                #print(race_weight_batch)
-                train_race_sess.run(training_op_race, feed_dict={X: X_batch, y: y_batch, training: True, loss_weight:race_weight_batch})
-            acc_batch = accuracy_race.eval(feed_dict={X: X_batch, y: y_batch, loss_weight:np.ones(len(y_batch))})
-            acc_train = accuracy_race.eval(feed_dict={X: X_train[1:100], y: y_race_train[1:100], loss_weight:np.ones(100)})
-            acc_test = accuracy_race.eval(feed_dict={X: X_test, y: y_race_test, loss_weight:np.ones(len(y_race_test))})
+                y_batch = y_train[it*batch_size:(it+1)*batch_size]
+                train_race_sess.run(training_op_race, feed_dict={X: X_batch, y: y_batch, training: True})
+            
+            acc_batch = accuracy_race.eval(feed_dict={X: X_batch, y: y_batch})
+            acc_train = accuracy_race.eval(feed_dict={X: X_train[1:100], y: y_train[1:100]})
+            acc_test = accuracy_race.eval(feed_dict={X: X_test, y: y_test})
             print(epoch, "Last batch accuracy:", acc_batch, "Train accuracy:", acc_train, "Test accuracy:", acc_test)
 
             save_path_race = saver.save(train_race_sess, RACE_PREDICTION_MODEL_PATH)
+
+
+def main():
+    X_train, X_test, y_race_train, y_race_test = prepare_race_data()
+    
+    race_encoder = create_race_encoder(y_race_train)
+    
+    y_train = race_encoder.transform(y_race_train)
+    y_test  = race_encoder.transform(y_race_test)
+    
+    X_train, y_train = over_sample(X_train, y_train)
+    
+    train_race_model(X_train, X_test, y_train, y_test, race_encoder)
 
 
 if __name__ == "__main__":
